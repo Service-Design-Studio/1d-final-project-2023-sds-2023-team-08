b6a43b83ccd53d9a1ad0f76593cbc287a1c69fcb
Vinny0712 <vinnykoh.1930@gmail.com>
2023-06-20 15:37:38 +0800

    added some comments, removed total_deposits methods from user model

--- backend-webapp/DBS-backend/app/models/transaction.rb
+++ backend-webapp/DBS-backend/app/models/transaction.rb
@@ -1,7 +1,7 @@
 class Transaction < ApplicationRecord
     belongs_to :account
 
-    def get_other_party_name
+    def get_other_party_name #helper method for generate transaction name below
         acc=Account.where(account_number: self.recipient_account_number)
         other_username=" "
         acc.each do |other_party_acc|  #this is cus .where return collection even tho only 1 user
@@ -12,7 +12,7 @@ class Transaction < ApplicationRecord
 
     end
 
-    def generate_transaction_name(outgoing)
+    def generate_transaction_name(outgoing) #used in user controller : all_transactions_desc
         if self.transaction_type=="Account Transfer"
             # "I-BANK TRANSFER OTHR"+transactionID e.g MB1242354363
             transaction_ID=1242004053+6*self.id
@@ -29,6 +29,9 @@ class Transaction < ApplicationRecord
 
         end
     end
+
+    
+
         
     
   
