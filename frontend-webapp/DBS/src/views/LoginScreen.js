5832bf100d409665d477a7139fcbbb5c17bac58f
Jaywhisker <51687528+Jaywhisker@users.noreply.github.com>
2023-06-22 17:56:03 +0800

    added alerts

--- frontend-webapp/DBS/src/views/LoginScreen.js
+++ frontend-webapp/DBS/src/views/LoginScreen.js
@@ -5,13 +5,31 @@ import { useNavigate } from 'react-router-dom';
 
 const Loginscreen = () => {
     const navigate = useNavigate();
+    const [empty, SetEmpty] = useState(false)
+
+    const handleSubmit = (event) => {
+        event.preventDefault(); // Prevent form submission
+    
+        const username = document.getElementById('username').value;
+        const pin = document.getElementById('pin').value;
+    
+        if (!username || !pin) {
+          SetEmpty(true)
+        } else {
+            //send code to backend??
+        }
+      };
+    
     
     return (
         <div className='loginbackground'>
             <img src="/assets/dbslogo.png" className='DBS'/>
-            <form className='form_styling'>
-                <input type="text" id="username" class="inputdata" placeholder="ENTER USERNAME" required/>
-                <input type="password" id="pin" class="inputdata" placeholder="ENTER PIN" required/>
+            <form className='form_styling' onSubmit={handleSubmit}>
+                {empty &&                 
+                <p className="alert">* Please fill in the required fields.</p>
+                }
+                <input type="text" id="username" class="inputdata" placeholder="ENTER USERNAME" />
+                <input type="password" id="pin" class="inputdata" placeholder="ENTER PIN" />
                 <p className='forgot'> Forgot your  <u>PIN</u>?</p>
                 <input type="submit" value="LOGIN" className='login'/>
             </form>
