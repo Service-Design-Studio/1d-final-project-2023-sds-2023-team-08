80661d90878b7722514771301a121d9a2a36b383
tristan <tris10yeo@gmail.com>
2023-06-27 14:37:53 +0800

    Refute Dispute Pages done

--- frontend-webapp/DBS/src/routes/Routes.js
+++ frontend-webapp/DBS/src/routes/Routes.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomeScreen from '../views/HomeScreen.js';
 import Recenttransaction from '../views/RecentTransaction.js';
+import RefuteDispute from '../views/RefuteDispute.js'
+import ReviewTransfer from '../views/ReviewTransfer.js';
 //import RouterSpy from './path-to-router-spy'
 
 const Routing = () => {
@@ -10,9 +12,9 @@ const Routing = () => {
     <Router>
       <Routes>
         <Route path="/" element={<HomeScreen />} />
-        <Route path="/invest" element={<HomeScreen />} />
+        <Route path="/invest" element={<RefuteDispute />} />
         <Route path="/pay" element={<HomeScreen />} />
-        <Route path="/plan" element={<HomeScreen />} />
+        <Route path="/plan" element={<ReviewTransfer />} />
         <Route path="/more" element={<HomeScreen />} />
         <Route path="/recenttransaction" element={<Recenttransaction />} />
         <Route path="/recenttransaction/:accountNumber" element={<Recenttransaction />} />
