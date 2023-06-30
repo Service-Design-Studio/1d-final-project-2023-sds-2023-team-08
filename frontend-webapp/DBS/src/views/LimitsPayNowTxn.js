9e1ce20580fba19f7e7f70a55462ac7f507c59ff
valenciaaaalim <valencia_arlin@mymail.sutd.edu.sg>
2023-06-30 19:17:34 +0800

    json linked

--- frontend-webapp/DBS/src/views/LimitsPayNowTxn.js
+++ frontend-webapp/DBS/src/views/LimitsPayNowTxn.js
@@ -1,8 +1,13 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import '../components/styles/LimitsPayNowTxnStyles.css';
+import paynowdetailsjson from '../testdata/paynowdetails.json';
+
 
 const LimitsPayNowTxn = () => {
     const navigate = useNavigate();
+    const { userID, accountNumber } = useParams();
+    const paynowtxn = paynowdetailsjson[0];
+
     return(
         <div classname = "overallPNT">
             <div className='padforRF1'>
@@ -16,9 +21,9 @@ const LimitsPayNowTxn = () => {
                 <div className='recipient_container1'>
                     <div className= 'profile1'></div>
                     <div className='account_right1'>
-                        <p className= 'accountname1'>RECIPIENT ACCOUNT</p>
+                        <p className= 'accountname1'>{paynowtxn.recipientdetails["recipient name"]}</p>
                         <div className= 'accountnumber1'>
-                            <p className= 'accountnumber1'>XXX-XXXXX-X</p>
+                            <p className= 'accountnumber1'>{paynowtxn.recipientdetails["recipient account number"]}</p>
                         </div>  
                     </div>
                 </div>
@@ -26,9 +31,9 @@ const LimitsPayNowTxn = () => {
                 <div className = 'sender_container1'>
                     <div className= 'profile2'></div>
                     <div className='account_right1'>
-                        <p className= 'accountname1'>SENDER ACCOUNT</p>
+                        <p className= 'accountname1'>{paynowtxn.senderdetails["sender name"]}</p>
                         <div className= 'accountnumber1'>
-                            <p className= 'accountnumber1'>XXX-XXXXX-Y</p>
+                            <p className= 'accountnumber1'>{paynowtxn.senderdetails["sender account number"]}</p>
                         </div>  
                     </div>
                 </div>
@@ -43,7 +48,7 @@ const LimitsPayNowTxn = () => {
                     <p className='currency1'>SGD</p>
                 </div>
                 <div className='rightside1'>
-                    <p className='refundamount1'>XX.XX</p>
+                    <p className='refundamount1'>{paynowtxn.txndetails["amount transacted"]}</p>
                 </div>
             </div>
 
@@ -53,19 +58,19 @@ const LimitsPayNowTxn = () => {
                     <p className='remainingPNT'>Remaining</p>
                 </div>
                 <div className='rightPNT'>
-                    <p className='AcategoryamountPNT'>SGD 40,000.00</p>
-                    <p className='RcategoryamountPNT'>SGD 40,000.00</p>
+                    <p className='AcategoryamountPNT'>SGD {paynowtxn.senderdetails["authorised limit"]}</p>
+                    <p className='RcategoryamountPNT'>SGD {paynowtxn.senderdetails["remaining limit"]}</p>
                 </div>
             </div>
 
             <p className='changelimitPNT'><u>Change Limit</u></p>
             <p className='trfdeetsPNT'>TRANSFER DETAILS</p>
             <p className='addcommentsPNT'>Add comments for recipient</p>
-            <p className='commentsPNT'>PayNow Transfer</p>
+            <p className='commentsPNT'>{paynowtxn.txndetails["comments for txn"]}</p>
 
             <div className='greyboxPNT'>
                 <p className='tncforrefund1'>By clicking "NEXT", you agree to be bound by the <u>Terms and Conditions.</u></p>
-                <button id='submitrefund1' className='submitbutton1' onClick={()=>{}}>SUBMIT</button>
+                <button id='submitrefund1' className='LIMITSbutton1' onClick={()=>{}}>NEXT</button>
             </div>        
         </div>
     );
--- frontend-webapp/DBS/src/views/RefuteSuccess.js
+++ frontend-webapp/DBS/src/views/RefuteSuccess.js
@@ -1,11 +1,16 @@
-import { useNavigate } from 'react-router-dom';
 import '../components/styles/RefuteSuccessStyles.css';
+import { useNavigate, useParams } from 'react-router-dom';
+import ftdrecipientjson from '../testdata/ftdrecipient.json';
+
 
 const RefuteSuccess = () => {
     const navigate = useNavigate();
+    const { userID, accountNumber } = useParams();
+    const FTDtransactions = ftdrecipientjson[0];
+ 
     return(
         <div className='overall1'>
-            <button id= 'closebutton' className='closebuttonRS' onClick={()=>{}}>X</button>
+            <button id= 'closebutton' className='closebuttonRS' onClick={()=>navigate(`/${userID}/home`)}>X</button>
 
             <div className='stampRS'>
                 <img src = './assets/greentick.png' className='greentickrefundRS'/>
@@ -16,32 +21,29 @@ const RefuteSuccess = () => {
             <div className='popboxRS'>
                 <div className='subpopboxRS'>
                     <p className='raisedonRS'>Raised On</p>
-                    <p className='ansRS'>DATE OF DISPUTE</p>
+                    <p className='ansRS'>{FTDtransactions.disputedate}</p>
                 </div>
                 <div className='subpopboxRS'>
                     <p className='qnRS'>Transaction Type</p>
-                    <p className='ansRS'>Funds Transfer</p>
+                    <p className='ansRS'>{FTDtransactions.transaction.transactiondetails["transaction type"]}</p>
                 </div>
                 <div className='subpopboxRS'>
                     <p className='qnRS'>Reason of Transfer Dispute</p>
-                    <p className='ansRS'>Transfer to Wrong Account</p>
+                    <p className='ansRS'>{FTDtransactions.transaction.FTDdetails["sender reason"]}</p>
                 </div>
                 <div className='subpopboxRS'>
-                    <p className='qnRS'>Comments from Recipient</p>
-                    <p className='ansRS'>Was supposed to transfer to xxx instead</p>
+                    <p className='qnRS'>Comments from Sender</p>
+                    <p className='ansRS'>{FTDtransactions.transaction.FTDdetails["sender comments"]}</p>
                 </div>
                 <div className='subpopboxRS'>
                     <p className='qnRS'>Reason for refuting dispute</p>
-                    <p className='uwuRS'>XXXXXXXXXXXXXXXXXXXXXXXX</p>
+                    <p className='uwuRS'>{FTDtransactions.transaction.FTDdetails["recipient comments"]}</p>
                 </div>
             </div>
             
             <p className='tncforrefund1'>The sender will be notified of your refute. No further action is required on your part.</p>
             <button id='closebutton' className='submitbutton1' onClick={()=>{}}>SHARE TRANSFER DETAILS</button>
         </div>
-
-
-
 
     );
 };
