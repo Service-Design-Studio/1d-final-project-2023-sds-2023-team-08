953519747ccb80ca9aaa1e673e8ea5825e83f6fc
valenciaaaalim <valencia_arlin@mymail.sutd.edu.sg>
2023-07-14 01:03:56 +0800

    cucumber2/3

--- frontend-webapp/DBS/features/f2.feature
+++ frontend-webapp/DBS/features/f2.feature
@@ -5,11 +5,64 @@ Feature: Log in page fail
     When I click "LOGIN" without entering details
     Then I will see a warning to fill in the inputs
     When I click "LOGIN" after entering details
-    Then feat2 I will be redirected to the homepage
+    Then I will be redirected to the homepage
 
   Scenario: I am navigating to the PayNow page
     Given I am at the homepage
     When I click on the PayNow icon
     Then I will be redirected to the PayNow Recipient page
+
+  Scenario: I am keying in the PayNow Recipient details fail
+    Given I am at the PayNow Recipient page
+    When I click "SUBMIT" before keying in a valid recipient
+    Then I will see a warning to key in a valid phone number 
+
+  Scenario: Filing a FTD after making a PayNow Transfer
+    Given I am at the PayNow Recipient page
+    When I key in my desired recipient's details
+    And I tap away to the empty space
+    Then the recipient's PayNow nickname will show up automatically in the field below
+    And I can click "SUBMIT"
+
+    And I will be redirected to the PayNow transaction page
+    When I have keyed in the amount I want to PayNow and I click "NEXT"
+    Then I will be redirected to the Review Transfer page
+    And it will reflect my bank account number
+    And it will reflect my recipient's phone number
+    And the comments is "Paynow Transfer"
+
+    When I click "NEXT"
+    Then I will be brought to a successful PayNow transfer page
+    And it will reflect my bank account number
+    And it will reflect my recipient's phone number
+    And the comments is "Paynow Transfer"
+
+    When I realise that I have sent to the wrong person and click on "Made a wrong transfer?"
+    Then I will be redirected to a "Raise A Fund Transfer Dispute" page
+    And it will reflect the amount transferred
+
+    When I click the "Raise Fund Transfer Dispute button" to confirm
+    Then I will receive a warning to select a valid reason
+
+    When I check the box for "Transfer to Wrong Account"
+    And I click the "Raise Fund Transfer Dispute button" to confirm
+    Then I will see a warning that the comments field cannot be blank
+
+    When I have keyed in my comments for recipient
+    And I click the "Raise Fund Transfer Dispute button" to confirm
+    Then I will be redirected to the review page
+    And the review will reflect my comments to the recipient
+    When I confirm to raise dispute by clicking the red button
+    Then I will be redirected to the successfully raised FTD page
+
+    When I press the x button
+    Then I will be redirected to the homepage
+
+  Scenario: Check if my FTD has been logged successfully
+    Given I am at the homepage
+    And I navigated to the Recent Transactions page
+    When I click onto the FTD status tab
+    Then I should see the FTD page
+    And I should see that the status is "AWAITING ACTION" of the transaction I made with amount "-12.00"
 
 
---
+++ frontend-webapp/DBS/features_bton/step_defs3.js
@@ -0,0 +1,213 @@
+const assert = require('assert');
+const { Given, When, Then, Before, After } = require('@cucumber/cucumber');
+const { Builder, By, Key, until } = require('selenium-webdriver');
+const { useEffect } = require('react');
+const { expect } = require('chai');
+
+Before(async function () {
+  // Set up the Selenium WebDriver
+  this.driver = await new Builder().forBrowser('chrome').build();
+});
+
+After(async function () {
+  // Quit the Selenium WebDriver after each scenario
+  await this.driver.quit();
+});
+
+Given('I am at the log in page', async function () {
+    // Navigate to the login page
+    await this.driver.get('http://localhost:3000');
+    await this.driver.manage().window().setRect({ width: 393, height: 851 });
+  });
+
+//////////////// Logging in to my account //////////////////////////////////////////////////////////////////////////////////////////////
+
+When('I click "LOGIN" after entering details', async function () {
+    const usernameField = await this.driver.findElement(By.id('username'))
+    usernameField.sendKeys("wei xuan")
+    const passwordField = await this.driver.findElement(By.id('pin'))
+    passwordField.sendKeys("password123")
+
+    const loginButton = await this.driver.findElement(By.className('login'));
+
+    // Add a delay of 1 second before clicking the button
+    await new Promise(resolve => setTimeout(resolve, 1000));
+  
+    // Click the button
+    await loginButton.click();
+  });
+  
+Then('feat2 I will be redirected to the homepage', async function () {
+    // Check if the current page is the home page
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    const currentUrl = await this.driver.getCurrentUrl();
+    assert.strictEqual(currentUrl, 'http://localhost:3000/5/home');
+});
+
+//////////////// Log in page fail //////////////////////////////////////////////////////////////////////////////////////////////
+
+
+When('I click "LOGIN" without entering details', async function () {
+  const loginButton = await this.driver.findElement(By.className('login'));
+
+  // Add a delay of 1 second before clicking the button
+  await new Promise(resolve => setTimeout(resolve, 1000));
+
+  // Click the button
+  await loginButton.click();
+});
+
+Then('I will see a warning to fill in the inputs', async function () {
+  // Check if there is a warning
+  const loginWarning = await this.driver.findElement(By.className('alert'));
+  const warningText = await loginWarning.getText();
+
+  // Assert the text content matches a particular string
+  expect(warningText).to.equal('* Please fill in the required fields.');
+});
+
+
+//////////////// I am at Home and I have a notification //////////////////////////////////////////////////////////////////////////////////////////////
+
+Given('I am at Home page', async function () {
+    // Navigate to the login page
+    await this.driver.get('http://localhost:3000/5/home');
+    await this.driver.manage().window().setRect({ width: 393, height: 851 });
+  });
+
+  When('I click on Resolve Now', async function () {
+    const resolveNowButton = await this.driver.findElement(By.className('buttonText'));
+  
+    // Add a delay of 1 second before clicking the button
+    await new Promise(resolve => setTimeout(resolve, 1000));
+  
+    // Click the button
+    await resolveNowButton.click();
+  });
+  
+Then('I will be redirected to the FTD Page', async function () {
+    // Check if the current page is the home page
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    const currentUrl = await this.driver.getCurrentUrl();
+    assert.strictEqual(currentUrl, 'http://localhost:3000/5/FTDtransactionsall');
+});
+//////////////// I am at the Fund Dispute page //////////////////////////////////////////////////////////////////////////////////////////////
+
+Given('I am at the Fund Dispute page', async function () {
+    // Navigate to the Fund Dispute Page
+    await this.driver.get('http://localhost:3000/5/FTDtransactionsall');
+    await this.driver.manage().window().setRect({ width: 393, height: 851 });
+  });
+
+
+  When('I click on Action Required as the Recipient', async function () {
+    const actionRequiredButton = await this.driver.findElement(By.className('statustext'));
+  
+    // Add a delay of 1 second before clicking the button
+    await new Promise(resolve => setTimeout(resolve, 1000));
+  
+    // Click the button
+    await actionRequiredButton.click();
+  });
+  
+Then('I will be redirected to the Dispute details Page', async function () {
+    // Check if the current page is the Fund Dispute Page
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    const currentUrl = await this.driver.getCurrentUrl();
+    assert.ok(currentUrl.includes('http://localhost:3000/5'));
+});
+
+Then("it will reflect the amount being disputed", async function () {
+    const disputeamt = await this.driver.findElement(By.className('moneydisputein'));
+    const amt = await disputeamt.getText();
+  
+    // Assert the text content matches a particular string
+    expect(amt).to.equal('12.00')
+
+});
+
+Then("it will reflect the comments made to me", async function () {
+    const commentsbox = await this.driver.findElement(By.id('commentstome'));
+    const commentsreview = await commentsbox.getText();
+  
+    // Assert the text content matches a particular string
+    expect(commentsreview).to.equal('Sorry! Supposed to send to someone else')
+});
+//////////////// I am feeling nice and I will return the money //////////////////////////////////////////////////////////////////////////////////////////////
+
+  When('I click on the Yes - Refund button', async function () {
+    const refundButton = await this.driver.findElement(By.className('refundbutton'));
+  
+    // Add a delay of 1 second before clicking the button
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    // Click the button
+    await refundButton.click();
+
+  });
+  
+Then('I will be redirected to the Refund Dispute page', async function () {
+    // Check if the current page is Refund Dispute page
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    const currentUrl = await this.driver.getCurrentUrl();
+    assert.ok(currentUrl.includes('http://localhost:3000/5/refunddispute'));
+});
+
+
+//////////////// I am reviewing the fund transfer //////////////////////////////////////////////////////////////////////////////////////////////
+
+  When('I click on the Submit button', async function () {
+    const submitButton = await this.driver.findElement(By.id('submitrefund1'));
+  
+    // Add a delay of 1 second before clicking the button
+    await new Promise(resolve => setTimeout(resolve, 1000));
+  
+    // Click the button
+    await submitButton.click();
+  });
+  
+Then('I will be redirected to the Review Transfer page', async function () {
+    // Check if the current page is the home page
+    const currentUrl = await this.driver.getCurrentUrl();
+    assert.strictEqual(currentUrl, 'http://localhost:3000/5/review');
+});
+
+
+//////////////// I am sure that the details are correct //////////////////////////////////////////////////////////////////////////////////////////////
+
+  When('I click on Transfer Now', async function () {
+    const transferNowButton = await this.driver.findElement(By.className('TransferNow'));
+  
+    // Add a delay of 1 second before clicking the button
+    await new Promise(resolve => setTimeout(resolve, 1000));
+  
+    // Click the button
+    await transferNowButton.click();
+
+  });
+  
+Then('I will be redirected to the Successful transfer page', async function () {
+    // Check if the current page is the home page
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    const currentUrl = await this.driver.getCurrentUrl();
+    assert.strictEqual(currentUrl, 'http://localhost:3000/5/success');
+});
+
+
+
+//////////////// I have refund the money and am going back to the home page //////////////////////////////////////////////////////////////////////////////////////////////
+
+  When('I click on Exit button', async function () {
+    const exitButton = await this.driver.findElement(By.className('successtxtransparent'));
+    // Add a delay of 1 second before clicking the button
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    // Click the button
+    await exitButton.click();
+  });
+  
+Then('I will be redirected to the Home page', async function () {
+    // Check if the current page is the review transfer page
+    const currentUrl = await this.driver.getCurrentUrl();
+    assert.strictEqual(currentUrl, 'http://localhost:3000/5/home');
+});
