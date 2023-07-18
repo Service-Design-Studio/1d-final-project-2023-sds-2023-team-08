cd13ceab5d8ccc7cc65fb0fd7cc39fcf14519814
Jaywhisker <51687528+Jaywhisker@users.noreply.github.com>
2023-07-18 23:49:00 +0800

    added clipboard

--- frontend-webapp/DBS/src/views/bank account/BankAccRecipient.js
+++ frontend-webapp/DBS/src/views/bank account/BankAccRecipient.js
@@ -7,29 +7,63 @@ import { useState, useEffect } from 'react';
 const BankAccRecipientScreen = () => {
     const navigate = useNavigate();
     const { userID } = useParams();
-    const [recipientName, setRecipientName] = useState()
-    const [recipientAccNum, setRecipientAccnum] = useState()
-    const [bankType, setbankType] = useState()
+    const [recipientName, setRecipientName] = useState('');
+    const [recipientAccNum, setRecipientAccnum] = useState('');
+    const [clipboardText, setClipboardText] = useState('');
+    const [showBottomSection, setShowBottomSection] = useState(false);
+    const [warningMessage, setWarningMessage] = useState('')
     const location = useLocation()
+    const bankType = location.state
     
-    setbankType(location.state)
-          
+    useEffect(() => {
+        const handleReadClipboard = async () => {
+          try {
+            const text = await navigator.clipboard.readText();
+            setClipboardText(text);
+            console.log(clipboardText)
+          } catch (error) {
+            console.error('Failed to read clipboard:', error);
+          }
+        };
+    
+        handleReadClipboard();
+      }, [recipientName, recipientAccNum]); 
+    
+    
     const handleSubmit = async(event) => {
-        event.preventDefault();            
-      
+        event.preventDefault();
+        if (recipientName.length == 0) {
+            setWarningMessage('* Please fill in your Recipient Name')
+        } else if (bankType == undefined) {
+            setWarningMessage('* Please chose a Bank')
+        } else if (recipientAccNum.length == 0) {
+            setWarningMessage('* Please fill in your Recipient Bank Account Number')
+        }
     };
 
     const handleInputChangeName = (e) => {
-        setRecipientName(e.target.value)
+        const inputValue = e.target.value;
+        const cleanedValue = inputValue.replace(/[^a-zA-Z\s]/g, '');
+        setRecipientName(cleanedValue);
+      };
+      
+
+    const handleInputChangeAccNum = (e) => {
+        setShowBottomSection(false)
+        setRecipientAccnum(e.target.value)
+    };
+
+    const setAutofill = () => {
+        const cleaned_clipboardText = clipboardText.replace(/-/g, '') 
+        setRecipientAccnum(cleaned_clipboardText)
+        setShowBottomSection(false)
     };
     
-    const blockInvalidChar =(e) => {
-        const input = e.target.value;
-        const regex = /^[a-zA-Z\s]*$/; 
-        if (!regex.test(input)) {
-          e.preventDefault(); 
+    const readClipBoard = async () =>{
+        if (clipboardText.length >= 7 && clipboardText.length <=11 && /^[0-9-]+$/.test(clipboardText) && recipientAccNum.length == 0) {
+            setShowBottomSection(true)
         }
-    }
+    };
 
     return(
         <div className = 'overall'>
@@ -43,11 +77,16 @@ const BankAccRecipientScreen = () => {
             <p className='pntentermobileER'>ENTER ACCOUNT DETAILS</p>
 
             <div className = 'bankacccontainer'>
+                { warningMessage.length > 0 && (
+                    <p  className='warningbankmessage'>{warningMessage}</p>
+                )}
                     <form className='formcontainer'>
                         <input
                             type="text"
                             className="bankaccdetails"
                             placeholder="Enter recipient's name"
+                            value={recipientName}
+                            onInput={handleInputChangeName}
                             />
                     </form>
 
@@ -62,13 +101,19 @@ const BankAccRecipientScreen = () => {
                             type="text"
                             className="bankaccdetails"
                             placeholder="Enter account no."
-                            onKeyDown={blockInvalidChar}
-                            value={recipientName}
-                            onInput={handleInputChangeName}
+                            value={recipientAccNum}
+                            onInput={handleInputChangeAccNum}
+                            onFocus={readClipBoard}
                             />
                     </form>
-            </div>
 
+                    { showBottomSection && 
+                        <button className='autofillcontainer' onClick={setAutofill}>
+                            <p className='bankaccnumbers'>{clipboardText}</p>
+                        </button>
+                    }
+            </div>
+            
             <button id='pntsubmitbutton' className='pntsubmitbuttonER' onClick={handleSubmit}>
                 <p className='pntsubmitER'>NEXT</p>
             </button>
