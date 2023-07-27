9d3b8f761fe49b5d8235559fea1d7f0332f2d041
btonyip <brightonyip@gmail.com>
2023-07-27 22:29:38 +0800

    commit

--- frontend-webapp/DBS/features/step_definition/init.js
+++ frontend-webapp/DBS/features/step_definition/init.js
@@ -21,7 +21,7 @@ Before(async function () {
   //declaring global variable for each scenario
   global.driver = initDriver()
   global.baseUrl = "http://localhost:3000/"
-  global.baseUrl = "https://dbsservice-zwrzqgoagq-as.a.run.app"
+  // global.baseUrl = "https://dbsservice-zwrzqgoagq-as.a.run.app"
   global.parentNumber = Math.floor(Math.random() * 100000000)
   global.childNumber = Math.floor(Math.random() * 100000000)
 
