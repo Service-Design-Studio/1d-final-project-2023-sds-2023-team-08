eb58ce5f9c404eee0d1c786f98381e9b7f2e54c9
Vinny0712 <vinnykoh.1930@gmail.com>
2023-08-01 00:11:03 +0800

    converted into gateway

--- backend-webapp/DBS-backend/app/controllers/users_controller.rb
+++ backend-webapp/DBS-backend/app/controllers/users_controller.rb
@@ -1,32 +1,52 @@
-
+require 'net/http'
+require 'json'
 class UsersController < ApplicationController
   
-  before_action :set_user, only: %i[ show edit update destroy default_acc list_accounts total_deposit all_transactions home]
+  
 
   
   def home
+
+
+    id=params[:id]
+    # Assuming you have the URL for the GET request
+    url = URI("https://dbs-cloudsql-service-5qwlwvimaq-as.a.run.app/users/#{id}/home")
+
+    # Create a new Net::HTTP object with SSL enabled (if needed)
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    # Set the read timeout in seconds (e.g., 10 seconds)
+    http.read_timeout = 10
+
+    # Send the GET request (no request body needed for GET requests)
+    response = http.get(url)
+    
+    
+    render json: response.body
     
 
-    data = {
-      "user": @user.username,
-      "account": @user.list_of_acc_infos
-      
 
-    }
-  
-    render json: data, status: :ok #http 200
   end
 
 
   def all_transactions
-    #outgoing
-    outgoing_7_days=@user.transactions.where("datetime >= ?", 7.days.ago.to_date)
-    
-    #incoming
-    incoming_7_days=@user.incoming_transactions.where("datetime >= ?", 7.days.ago.to_date)
 
-    data=self.all_transactions_desc(outgoing_7_days,incoming_7_days)
-    render json: data, status: :ok #http 200
+    id=params[:id]
+    
+    # Assuming you have the URL for the GET request
+    url = URI("https://dbs-cloudsql-service-5qwlwvimaq-as.a.run.app/users/#{id}/all_transactions")
+
+    # Create a new Net::HTTP object with SSL enabled (if needed)
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    # Set the read timeout in seconds (e.g., 10 seconds)
+    http.read_timeout = 10
+
+    # Send the GET request (no request body needed for GET requests)
+    response = http.get(url)
+    
+    
+    render json: response.body
 
 
   end
@@ -35,146 +55,68 @@ class UsersController < ApplicationController
   def login
     
     
-    begin
-      user_params = params.require([:username, :pin])
-      
-    rescue ActionController::ParameterMissing => e
-      render json: { success: false, error: e.message }, status: :unprocessable_entity
-      return
-    end  
+    raw=request.raw_post
+    puts raw
     
-    user=User.where(username: user_params[0], password: user_params[1]).first
     
-    if user
-      
-      render json: { success: true, userid:user.id }, status: :ok
-    else
-      
-      render json: { success: false, error: "no user found" }, status: :unprocessable_entity #http 422 
-    end
+
+    # Assuming you have the URL for the GET request
+    url = URI('https://dbs-cloudsql-service-5qwlwvimaq-as.a.run.app/csrf_token')
+
+    # Create a new Net::HTTP object with SSL enabled (if needed)
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    # Set the read timeout in seconds (e.g., 10 seconds)
+    http.read_timeout = 10
+
+    # Send the GET request (no request body needed for GET requests)
+    response = http.get(url)
+
+    body=JSON.parse(response.body)
+    csrf_token = body["csrfToken"]
+
+    #url and http setup
+    url = URI('https://dbs-cloudsql-service-5qwlwvimaq-as.a.run.app/users/login')
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    
+    
+    
+    #create post req
+    request = Net::HTTP::Post.new(url)
+    request['Content-Type'] = 'application/json'
+    request['X-CSRF-Token'] = csrf_token
+    request.body = raw
+
+    #send POST and get response
+    response = http.request(request)
+    
+
+    #return response
+    render json: response.body
+  
 
   end
 
   # GET 'users/:id/default_acc'
   def default_acc
-    default_acc=@user.accounts.first
-    if default_acc
-      data={
-        default_acc_name:default_acc.account_type,
-        default_acc_number:default_acc.account_number
-      }
-      render json: data, status: :ok #http 200
-    else
-      data={
-        success:false,
-        error:"no account exist"
-      }
-      render json: data, status: :unprocessable_entity 
-    end
-
-  end
-
-  # GET /users or /users.json
-  def index
-    @users = User.all
-  end
-
-
-  # GET /users/1 or /users/1.json
-  def show
-  end
-
-  # GET /users/new
-  def new
-    @user = User.new
-  end
-
-  # GET /users/1/edit
-  def edit
-  end
-
-  # POST /users or /users.json
-  def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to user_url(@user), notice: "User was successfully created." }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /users/1 or /users/1.json
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /users/1 or /users/1.json
-  def destroy
-    @user.destroy
-
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: "User was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    rescue ActiveRecord::RecordNotFound
-      raise ActionController::RoutingError, 'User Not Found'
-      #render json: { error: "user not found" }, status: :not_found  #404 Not Found
-    end
-
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.fetch(:user, {})
-    end
-
     
-  def all_transactions_desc(outgoing,incoming) #latest to earliest
-        
-  
-    #combined_sorted=(outgoing.to_a + incoming.to_a).sort_by(&:datetime).reverse #latest to earliest
-    combined_sorted=(outgoing.to_a + incoming.to_a)
-    combined_sorted=combined_sorted.sort_by { |t| DateTime.parse(t.date_time) }.reverse
+    id=params[:id]
+    # Assuming you have the URL for the GET request
+    url = URI("https://dbs-cloudsql-service-5qwlwvimaq-as.a.run.app/users/#{id}/default_acc")
 
-    data=[]
-    combined_sorted.each do |transaction|
-      outgoing= (transaction.account.user.id==@user.id) 
-      data.push(
-        {
-          "date":transaction.datetime.strftime("%a, %d %b %Y"),
-          "transaction": {
-            "transaction name": transaction.generate_transaction_name(outgoing) ,
-            "mode of payment": transaction.transaction_type,
-            "transaction type": "FAST / PayNow Transfer",
-            "account number": outgoing ? transaction.account.account_number : transaction.recipient_account_number,
-            #if the account which made this transaction belongs to the user, it means outgoing funds thus other party is recipient acc, else sender's acc 
-            "total amount": outgoing ? -transaction.amount : transaction.amount,
-            #if the account which made this transaction belongs to the user, it means outgoing funds thus -ve, else +ve
-            "transaction ID": transaction.id,
-            
-          }
+    # Create a new Net::HTTP object with SSL enabled (if needed)
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    # Set the read timeout in seconds (e.g., 10 seconds)
+    http.read_timeout = 10
 
-        }
-      )
-    end
-    return data
+    # Send the GET request (no request body needed for GET requests)
+    response = http.get(url)
+    
+    
+    render json: response.body
+    
 
   end
 
--- backend-webapp/DBS-backend/config/routes.rb
+++ backend-webapp/DBS-backend/config/routes.rb
@@ -36,10 +36,10 @@ Rails.application.routes.draw do
   post 'users/:id/transactions/:transactions_id/withdraw_dispute', to: 'disputes#withdraw_dispute'
   post 'users/:id/transactions/:transactions_id/refute_dispute', to: 'disputes#refute_dispute'
   post 'users/:id/transactions/:transactions_id/resolve_dispute', to: 'disputes#resolve_dispute'
-  get 'users/:id/transaction/:transactions_id/details_for_ftd_review', to:'disputes#details_for_ftd_review'
+  
   get 'user/:id/transaction_detail_for_disputes_involving_user', to: 'disputes#transaction_detail_for_disputes_involving_user'
   get 'users/:id/transactions/:transactions_id/refund_details', to: 'disputes#refund_details'
- post 'users/:id/disputes/create', to: 'disputes#create'
+ 
   #for paynow
   get 'users/:id/paynows/search_by_phone/:phonenumber', to: 'paynows#search_by_phone'
  
